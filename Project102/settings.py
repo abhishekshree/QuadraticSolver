b558fa2b45a54649a94dc79acf694ee978bb586f
Abhishek Shree <abhi1234456@gmail.com>
2018-05-11 14:17:55 +0530

    Changes for upload

--- Project102/settings.py
+++ Project102/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'klz%09nprfvgaydu7a0jj7yi+b!ajrok3t^a4@&zn7j#gv5^7('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
